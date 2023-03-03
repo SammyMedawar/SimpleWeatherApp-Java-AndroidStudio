0882dacb1fce3c4406d8375b62e1baa826fc2149
ayaelsaoudi01 <ayasphoneemail2001@gmail.com>
2023-03-03 18:18:45 +0200

    finished project

---
+++ app/src/main/java/com/example/sammysweatherapp/Models/OneDay.java
@@ -0,0 +1,13 @@
+package com.example.sammysweatherapp.Models;
+
+public class OneDay {
+    public ThreeHours[] getHours() {
+        return hours;
+    }
+
+    public void setHours(ThreeHours[] hours) {
+        this.hours = hours;
+    }
+
+    public ThreeHours[] hours;
+}
